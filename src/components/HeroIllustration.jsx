e636d92081fd48dc7e93751cd053e87ba5b4e686
munirajgowda <munirajgowdaraj@gmail.com>
2026-03-13 20:45:55 +0530

    update

--- src/components/HeroIllustration.jsx
+++ src/components/HeroIllustration.jsx
@@ -23,51 +23,6 @@ const HeroIllustration = () => {
           style={{ width: '100%', height: 'auto', objectFit: 'contain' }}
         />
       </div>
-
-      {/* Name Badge */}
-      <div 
-        className="sticker sticker-orange"
-        style={{ 
-          position: 'absolute', 
-          top: 40, 
-          right: -10, 
-          zIndex: 10,
-          fontSize: '20px',
-          textTransform: 'uppercase'
-        }}
-      >
-        Muniraju B R
-      </div>
-
-      {/* Available for Work Badge */}
-      <div 
-        className="sticker sticker-blue"
-        style={{ 
-          position: 'absolute', 
-          bottom: 40, 
-          right: 20, 
-          zIndex: 10,
-          fontSize: '16px',
-          gap: '12px'
-        }}
-      >
-        {/* Verification Icon */}
-        <div style={{ 
-          width: 24, 
-          height: 24, 
-          background: 'var(--bg)', 
-          borderRadius: '50%', 
-          border: '2px solid black',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}>
-          <svg width="12" height="10" viewBox="0 0 12 10" fill="none">
-            <path d="M1 5L4.5 8.5L11 1" stroke="black" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-          </svg>
-        </div>
-        Available for work
-      </div>
     </div>
   );
 };
